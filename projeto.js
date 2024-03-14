40d530f3a55f6aedac34ea2daabcddb7a7c4737f
Seu Nome <seu@email.com>
2024-03-13 21:25:25 -0300

    Nova atualização do projeto

--- projeto.js
+++ projeto.js
@@ -28,12 +28,40 @@ Ao final deve se exibir uma mensagem:
 "O Herói de nome **{nome}** está no nível de **{nivel}**"
 */ 
 
-let heroi = {nome: 'Aquaman', xp: 5000};
-let nivelPrata = 'Prata';
+// HERÓIS
+let heroAquaman = {nome: 'Aquaman', xp: 6.024}
+let heroSuperman = {nome: 'SuperMan', xp: 10.001}
+let heroBatman = {nome: 'Batman', xp: 7.000}
+let heroHomemAranha = {nome: 'Homem Aranha', xp: 5.000}
+// NÍVEIS DE XP
+let niveisDeXP = ['Ferro', 'Bronze', 'Prata', 'Ouro', 'Platina', 'Ascendente', 'Imortal', 'Radiante'] 
 
-if (heroi.xp === 5000){
-    console.log(`O herói ${heroi.nome} está no nível ${nivelPrata}`);
-}
+    if (heroAquaman.xp === 1.000){
+        console.log(`O herói de nome ${heroAquaman.nome} está no nível de ${niveisDeXP[0]}`)
+    }
+    else if ((heroAquaman.xp >= 1.001) && (heroAquaman.xp <= 2.000)){
+        console.log(`O herói de nome ${heroAquaman.nome} está no nível de ${niveisDeXP[1]}`)
+    }
+    else if((heroAquaman.xp >= 2.001)&& ( heroAquaman.xp <= 5.000)){
+        console.log(`O herói de nome ${heroAquaman.nome} está no nível de ${niveisDeXP[2]}`)
+    }
+    else if ((heroAquaman.xp >= 5.001) && (heroAquaman.xp <= 7.000)){
+        console.log(`O herói de nome ${heroAquaman.nome} está no nível de ${niveisDeXP[3]}`)
+    }
+    else if ((heroAquaman.xp  >= 7.001) && (heroAquaman.xp  <= 8.000)) {
+        console.log(`O herói de nome ${heroAquaman.nome} está no nível de ${niveisDeXP[4]}`)
+    }
+    else if ((heroAquaman.xp >= 8.001) && (heroAquaman.xp <= 9.000)) {
+        console.log(`O herói de nome ${heroAquaman.nome} está no nível de ${niveisDeXP[5]}`)
+    }
+    else if ((heroAquaman.xp >= 9.001) && (heroAquaman.xp < 10.000)) {
+        console.log(`O herói de nome ${heroAquaman.nome} está no nível de ${niveisDeXP[6]}`)
+    } 
+    else if (heroAquaman.xp >= 10.001) {
+        console.log(`O herói de nome ${heroAquaman.nome} está no nível de ${niveisDeXP[7]}`)
+    }
+        
+        
 
 
 
